57fec91a70d004d3efb7c16921589f4ffa309adc
Mohamad Ahmadi <m.ahmadi@solutions-apps.de>
2023-04-12 19:28:55 +0330

    add changePasswordState

--- lib/presentation/viewModel/user/ChangePasswordViewModel.dart
+++ lib/presentation/viewModel/user/ChangePasswordViewModel.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mamak/presentation/state/formState/user/change_password_form_state.dart';
 import 'package:mamak/presentation/viewModel/baseViewModel.dart';
 
-class ChangePasswordViewModel extends BaseViewModel{
+class ChangePasswordViewModel extends BaseViewModel {
   ChangePasswordViewModel(super.initialState);
 
   final formKey = GlobalKey<FormState>();
+  ChangePasswordFormState formState = ChangePasswordFormState();
 
+
+
+  Function(String) get onOldPasswordChange =>
+          (mobile) => formState.password = mobile;
+
+  Function(String) get onPasswordChange =>
+          (mobile) => formState.confirmPassword = mobile;
 }
