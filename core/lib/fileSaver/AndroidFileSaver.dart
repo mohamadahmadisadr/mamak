6978d9257a01ba1968a6333f5858425985531b66
Mohamad <mohamad@mohamad.local>
2023-07-12 10:12:03 +0330

    upgrade some dependenciy

--- core/lib/fileSaver/AndroidFileSaver.dart
+++ core/lib/fileSaver/AndroidFileSaver.dart
@@ -1,16 +1,16 @@
 import 'dart:typed_data';
 
-import 'package:file_saver/file_saver.dart';
+// import 'package:file_saver/file_saver.dart';
 
 class AndroidFileSaver {
-  static Future<String> saveFile(Uint8List bytes) async {
-    var name = '${DateTime.now().microsecondsSinceEpoch}.pdf';
-    var path = await FileSaver.instance.saveFile(
-      name: name,
-      bytes: bytes,
-      mimeType: MimeType.pdf,
-      filePath: 'sdcard/Download/',
-    );
-    return path;
-  }
+  // static Future<String> saveFile(Uint8List bytes) async {
+  //   var name = '${DateTime.now().microsecondsSinceEpoch}.pdf';
+  //   var path = await FileSaver.instance.saveFile(
+  //     name: name,
+  //     bytes: bytes,
+  //     mimeType: MimeType.pdf,
+  //     filePath: 'sdcard/Download/',
+  //   );
+  //   return path;
+  // }
 }
--- feature/lib/downloadManager/DownloadManager.dart
+++ feature/lib/downloadManager/DownloadManager.dart
@@ -3,22 +3,22 @@ import 'dart:ui';
 
 import 'package:flutter_downloader/flutter_downloader.dart';
 
-class DownloadManager{
-  static void init() async{
+class DownloadManager {
+  static void init() async {
     await FlutterDownloader.initialize(
         debug: true,
         // optional: set to false to disable printing logs to console (default: true)
         ignoreSsl:
-        true // option: set to false to disable working with http links (default: false)
-    );
-    FlutterDownloader.registerCallback(downloadCallback);
+            true // option: set to false to disable working with http links (default: false)
+        );
+    FlutterDownloader.registerCallback((id, status, progress) => downloadCallback(id, status, progress),);
   }
 
   @pragma('vm:entry-point')
   static void downloadCallback(
-      String id, DownloadTaskStatus status, int progress) {
+      String id, int status, int progress) {
     final SendPort? send =
-    IsolateNameServer.lookupPortByName('downloader_send_port');
+        IsolateNameServer.lookupPortByName('downloader_send_port');
     send?.send([id, status, progress]);
   }
 }
